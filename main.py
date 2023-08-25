bcc93c092fde0faceb44cd618ad199e2537972af
Mindless Madness <professor.windu@gmail.com>
2023-08-26 01:01:54 +0530

    Implemented Data Ingestion

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from sensor.components.data_ingestion import DataIngestion
 from sensor.logger import logging
 from sensor.exception import SensorException
 import sys, os
+from sensor.utils import get_collection_as_dataframe
+from sensor.entity.config_entity import DataIngestionConfig
+from sensor.entity import config_entity
 
 if __name__=="__main__":
-    try:...
+    try:
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        print(data_ingestion.initiate_data_ingestion())
     except Exception as e:
         print(e)
---
+++ sensor/__init__.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+
+print("loading environment variables...")
+load_dotenv()
